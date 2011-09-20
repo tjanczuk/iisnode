c2d3bb9f23cef8457ef34564b3221af2036898fa
Tomasz Janczuk <tomasz@janczuk.org>
2011-09-20 15:11:32 -0700

    add node version information to helloworld sample

--- src/samples/helloworld/hello.js
+++ src/samples/helloworld/hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello, world! [helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ']');
+    res.end('Hello, world! [helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
 }).listen(process.env.PORT);  
