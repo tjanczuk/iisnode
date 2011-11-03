9e1199906edb44ac58f90cc742c9584d07a7c3de
Tomasz Janczuk <tomasz@janczuk.org>
2011-11-02 20:21:25 -0700

    debugger test

---
+++ test/functional/tests/109_helloworld_debug.js
@@ -0,0 +1,15 @@
+/*
+A simple GET request receives a hello world response
+*/
+
+var iisnodeassert = require("iisnodeassert");
+
+iisnodeassert.sequence([
+    iisnodeassert.get(10000, "/100_helloworld/hello.js/debug", 301),
+    iisnodeassert.get(10000, "/100_helloworld/hello.js/debug/", 200),
+    iisnodeassert.get(10000, "/100_helloworld/hello.js/debug/?foo", 200, "Unrecognized debugging command. Supported commands are ?debug, ?brk, and ?kill."),
+    iisnodeassert.get(10000, "/100_helloworld/hello.js/debug/?kill", 200, "The debugger and debugee processes have been killed."),
+    iisnodeassert.get(10000, "/100_helloworld/hello.js/debug/?kill", 200, "The debugger for the application is not running."),
+    iisnodeassert.get(10000, "/100_helloworld/hello.js/debug/?brk", 200),
+    iisnodeassert.get(10000, "/100_helloworld/hello.js/debug/?kill", 200, "The debugger and debugee processes have been killed.")
+]);
