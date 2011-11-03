0f517485e9142257af547c2535d12da4e45b13d7
Tomasz Janczuk <tomasz@janczuk.org>
2011-11-02 19:15:08 -0700

    fix 200_samples tests

--- test/functional/tests/parts/200_samples.js
+++ test/functional/tests/parts/200_samples.js
@@ -9,9 +9,9 @@ iisnodeassert.sequence([
     iisnodeassert.get(10000, "/node/configuration/hello.js", 200, "Hello, world! [configuration sample]"),
     iisnodeassert.get(10000, "/node/logging/hello.js", 200, "Hello, world! [logging sample]"),
     iisnodeassert.get(10000, "/node/defaultdocument/", 200, "You have reached the default node.js application at index.js! [defaultdocument sample]"),
-    iisnodeassert.get(10000, "/node/express/hello/foo", 200, "Hello from foo! [express sample]"),
-    iisnodeassert.get(2000, "/node/express/hello/bar", 200, "Hello from bar! [express sample]"),
-    iisnodeassert.get(2000, "/node/express/hello/idonotexist", 404, "Cannot GET /node/express/hello/idonotexist"),
-    iisnodeassert.get(10000, "/node/urlrewrite/hello/foo/bar/baz", 200, "Hello from urlrewrite sample. Request path: /node/urlrewrite/hello/foo/bar/baz"),
-    iisnodeassert.get(2000, "/node/urlrewrite/hello/1/2/3?param=4", 200, "Hello from urlrewrite sample. Request path: /node/urlrewrite/hello/1/2/3?param=4")
+    iisnodeassert.get(10000, "/node/express/myapp/foo", 200, "Hello from foo! [express sample]"),
+    iisnodeassert.get(2000, "/node/express/myapp/bar", 200, "Hello from bar! [express sample]"),
+    iisnodeassert.get(2000, "/node/express/myapp/idonotexist", 404, "Cannot GET /node/express/myapp/idonotexist"),
+    iisnodeassert.get(10000, "/node/urlrewrite/myapp/foo/bar/baz", 200, "Hello from urlrewrite sample. Request path: /node/urlrewrite/myapp/foo/bar/baz"),
+    iisnodeassert.get(2000, "/node/urlrewrite/myapp/1/2/3?param=4", 200, "Hello from urlrewrite sample. Request path: /node/urlrewrite/myapp/1/2/3?param=4")
 ]);
