8b5ef27c456ef23c208d8c2a3afd0a1f62539f83
Tomasz Janczuk <tomasz@janczuk.org>
2011-11-02 17:26:30 -0700

    adding debugger to samples

--- src/samples/express/hello.js
+++ src/samples/express/hello.js
@@ -2,11 +2,11 @@ var express = require('express');
 
 var app = express.createServer();
 
-app.get('/node/express/hello/foo', function (req, res) {
+app.get('/node/express/myapp/foo', function (req, res) {
     res.send('Hello from foo! [express sample]');
 });
 
-app.get('/node/express/hello/bar', function (req, res) {
+app.get('/node/express/myapp/bar', function (req, res) {
     res.send('Hello from bar! [express sample]');
 });
 
