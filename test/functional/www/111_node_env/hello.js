4934fa44b60fd012439bc91b0752fc42ec960283
Tomasz Janczuk <tomasz@janczuk.org>
2011-11-22 10:40:46 -0800

    enabling test case after node v0.6.2 fix for nul stdout

--- test/functional/www/111_node_env/hello.js
+++ test/functional/www/111_node_env/hello.js
@@ -1,8 +1,7 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    // TODO, tjanczuk, the following line requires a fix from node v0.6.2 which will be released tomorrow; uncomment
-    //console.log('logging to /dev/null');
+    console.log('logging to /dev/null');
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('Hello, world! [NODE_ENV]=' + process.env.NODE_ENV);
 }).listen(process.env.PORT);  
