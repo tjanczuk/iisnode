dae6b7913d1a41c8e914d65bc52c1e3287cf3c20
Tomasz Janczuk <tomasz@janczuk.org>
2011-12-13 13:17:33 -0800

    adding http handler to establish performance baseline for testing in iis

--- src/iisnode/cnodehttpmodule.cpp
+++ src/iisnode/cnodehttpmodule.cpp
@@ -5,6 +5,7 @@ CNodeHttpModule::CNodeHttpModule(CNodeApplicationManager* applicationManager)
 {
 }
 
+#if TRUE
 REQUEST_NOTIFICATION_STATUS CNodeHttpModule::OnExecuteRequestHandler(
 	IN IHttpContext* pHttpContext, 
 	IN IHttpEventProvider* pProvider)
@@ -65,6 +66,67 @@ Error:
 
 	return RQ_NOTIFICATION_FINISH_REQUEST;
 }
+#else
+
+// This section can be used to establish performance baseline 
+// This simple handler returns an HTTP 200 with a Hello, world! in the body
+
+REQUEST_NOTIFICATION_STATUS CNodeHttpModule::OnExecuteRequestHandler(
+	IN IHttpContext* pHttpContext, 
+	IN IHttpEventProvider* pProvider)
+{
+        UNREFERENCED_PARAMETER( pProvider );
+
+        // Create an HRESULT to receive return values from methods.
+        HRESULT hr;
+
+        // Retrieve a pointer to the response.
+        IHttpResponse * pHttpResponse = pHttpContext->GetResponse();
+
+        // Test for an error.
+        if (pHttpResponse != NULL)
+        {
+            // Clear the existing response.
+            pHttpResponse->Clear();
+            // Set the MIME type to plain text.
+            pHttpResponse->SetHeader(
+                HttpHeaderContentType,"text/plain",
+                (USHORT)strlen("text/plain"),TRUE);
+
+            // Create a string with the response.
+            PCSTR pszBuffer = "Hello World!";
+            // Create a data chunk.
+            HTTP_DATA_CHUNK dataChunk;
+            // Set the chunk to a chunk in memory.
+            dataChunk.DataChunkType = HttpDataChunkFromMemory;
+            // Buffer for bytes written of data chunk.
+            DWORD cbSent;
+
+            // Set the chunk to the buffer.
+            dataChunk.FromMemory.pBuffer =
+                (PVOID) pszBuffer;
+            // Set the chunk size to the buffer size.
+            dataChunk.FromMemory.BufferLength =
+                (USHORT) strlen(pszBuffer);
+            // Insert the data chunk into the response.
+            hr = pHttpResponse->WriteEntityChunks(
+                &dataChunk,1,FALSE,TRUE,&cbSent);
+
+            // Test for an error.
+            if (FAILED(hr))
+            {
+                // Set the HTTP status.
+                pHttpResponse->SetStatus(500,"Server Error",0,hr);
+            }
+
+            // End additional processing.
+            return RQ_NOTIFICATION_FINISH_REQUEST;
+        }
+
+        // Return processing to the pipeline.
+        return RQ_NOTIFICATION_CONTINUE;
+}
+#endif
 
 REQUEST_NOTIFICATION_STATUS CNodeHttpModule::OnAsyncCompletion(
 	IHttpContext* pHttpContext, DWORD dwNotification, BOOL fPostNotification, IHttpEventProvider* pProvider, IHttpCompletionInfo* pCompletionInfo)
