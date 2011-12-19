20ab81b961b5038f20838ddff0545ca1012d8d47
Tomasz Janczuk <tomasz@janczuk.org>
2011-12-19 14:48:07 -0800

    fix #111: add version information to iisnode.dll

--- src/iisnode/scripts/genVersionHeader.bat
+++ src/iisnode/scripts/genVersionHeader.bat
@@ -1,3 +1,5 @@
 @echo off
 for /F "tokens=*" %%i in (%~dp0\..\..\version.txt) do set IISNODE_VERSION=%%i
+set IISNODE_VERSION_LONG=%IISNODE_VERSION:.=,%
 echo #define IISNODE_VERSION ^"%IISNODE_VERSION%^"
+echo #define IISNODE_VERSION_LONG %IISNODE_VERSION_LONG%,0
