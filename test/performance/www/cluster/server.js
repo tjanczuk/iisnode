cfefd0a6ff8aacfeff4b0a7be702ea132629a415
Tomasz Janczuk <tomasz@janczuk.org>
2011-12-20 13:56:25 -0800

    multi process performance test

---
+++ test/performance/www/cluster/server.js
@@ -0,0 +1,26 @@
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello, world!');
+});
+
+if (!process.env.mode) {
+    server.listen(process.env.PORT);
+}
+else {
+    var cluster = require('cluster');
+
+    if (cluster.isMaster) {
+        for (var i = 0; i < 3; i++) {
+            cluster.fork();
+        }
+
+        cluster.on('death', function(worker) {
+            cluster.log('worker ' + worker.pid + ' died');
+        });
+    }
+    else {
+        server.listen(31416);
+    }
+}
