5cf74ab8c7f1903e4f8246a3647e0269ad405aa1
Tomasz Janczuk <tomasz@janczuk.org>
2011-12-21 15:22:02 -0800

    support for multiple client machines in performance client

--- test/performance/client.bat
+++ test/performance/client.bat
@@ -2,8 +2,13 @@
 
 set server=%1
 set scenario="%~dp0www\%2\wcat.ubr"
+set clients=localhost
+set vclients=2000
 
 set wcat="%programfiles%\WCAT\wcat.wsf"
+
+if [%3] neq [] set clients=%3
+if "%4" neq "" set vclients=%4
 
 if not exist %scenario% goto help
 
@@ -14,13 +19,13 @@ if not exist %wcat% (
 
 copy /y "%programfiles%\WCAT\report.xsl" "%~dp0"
 
-cscript %wcat% -terminate -update -clients localhost
+cscript %wcat% -terminate -update -clients %clients%
 if %ERRORLEVEL% neq 0 (
     echo FAILED. Error initializing WCAT client.
 	exit /b -1
 )
 
-cscript %wcat% -terminate -run -clients localhost -t %scenario% -s %server% -p 31416 -v 2000 -singleip -x
+cscript %wcat% -terminate -run -clients %clients% -t %scenario% -s %server% -p 31416 -v %vclients% -singleip -x
 if %ERRORLEVEL% neq 0 (
     echo FAILED. Error running WCAT client.
 	exit /b -1
@@ -30,8 +35,10 @@ exit /b 0
 
 :help
 
-echo Usage: client.bat ^<server^> ^<scenario^>
+echo Usage: client.bat ^<server^> ^<scenario^> ^[^"^<clients^"^> ^[^<vclients^>^]^]
 echo    ^<server^>   - server host name
 echo    ^<scenario^> - name of one of the subdirectories of %~dp0www
+echo    ^<clients^>  - comma delimited list of WCAT client machines to use; defaults to localhost
+echo    ^<vclients^> - number of virtual clients per physical client; defaults to 2000
 
 exit /b -1
