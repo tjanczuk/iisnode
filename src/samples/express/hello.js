b5ce54b4275e3f0ac8c8a717ecfe6cf8bd47a0bc
Joseph Gill <joseph.gill.atlanta@gmail.com>
2017-08-31 19:31:20 -0400

    changed url rewrite settings and api paths to include wildcard

--- src/samples/express/hello.js
+++ src/samples/express/hello.js
@@ -2,11 +2,11 @@ var express = require('express');
 
 var app = express.createServer();
 
-app.get('/node/express/myapp/foo', function (req, res) {
+app.get('*/myapp/foo', function (req, res) {
     res.send('Hello from foo! [express sample]');
 });
 
-app.get('/node/express/myapp/bar', function (req, res) {
+app.get('*/myapp/bar', function (req, res) {
     res.send('Hello from bar! [express sample]');
 });
 
